3100e9ce642c928c4782b0b5f7c842952d6d6d82
TechLady <kalengzonline@hotmail.com>
2016-09-02 01:13:23 +0800

    Change

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :movies do
   	resources :reviews, except: [:show, :index]
-   
-end
-
-devise_scope :user do  
-   get '/users/sign_out' => 'devise/sessions#destroy'  
-
   end
+
+	devise_scope :user do  
+	get '/users/sign_out' => 'devise/sessions#destroy'  
+	end
   
   root 'movies#index'
   end
